0f2a24502933f83a7ee008164200c1b349ee76e0
BEN1JEN <benpheard@gmail.com>
2020-02-04 22:44:24 -0700

    Add base javascript code

--- src/main.js
+++ src/main.js
@@ -1,2 +1,19 @@
-field = Image();
+"use strict";
+
+const field = Image();
 field.src = "field.png";
+
+let canvasContext;
+
+window.onload = () => {
+  const canvas = document.getElementById("canvas");
+	canvasContext = canvas.getContext("2d");
+  canvasContext.imageSmoothingEnabled = false;
+};
+
+setInterval(() => {
+  vpSize = {"x": innerWidth, "y": innerHeight};
+	document.getElementById("mainCanvas").width = vpSize.x;
+	document.getElementById("mainCanvas").height = vpSize.y;
+  canvasContext.drawImage(field);
+}, 16);
