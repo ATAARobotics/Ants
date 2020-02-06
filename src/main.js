3e41f81abf507b2f812634cd65e7b169cbc958a4
BEN1JEN <benpheard@gmail.com>
2020-02-05 21:51:28 -0700

    Add zooming

--- src/main.js
+++ src/main.js
@@ -11,16 +11,18 @@ window.onload = () => {
   const canvasContext = canvas.getContext("2d");
   const projectName = document.getElementById("project-name");
   canvasContext.imageSmoothingEnabled = false;
-  const vp = new Viewport();
-  vp.size = innerHeight*2;
+  const canvasSize = new Vec2(0, 0);
+  const vp = new Viewport(canvasSize);
+  vp.size = Math.min(canvas.offsetWidth, canvas.offsetHeight*2);
   setInterval(() => {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+    canvasSize.x = canvas.offsetWidth;
+    canvasSize.y = canvas.offsetHeight;
+    canvas.width = canvasSize.x;
+    canvas.height = canvasSize.y;
     const pos = vp.fromViewport(new Vec2(0, 0));
-    const pos2 = vp.fromViewport(new Vec2(0, 0.5));
     const size = vp.size;
     canvasContext.drawImage(field, pos.x-size/2, pos.y-size/4, size, size/2);
-    canvasContext.drawImage(field, pos2.x-size/2, pos2.y-size/4, size, size/2);
   }, 16);
   projectName.onkeyup = () => document.getElementById("title").innerText = `ANTS · ${projectName.value}`;
+  canvas.onwheel = ev => vp.zoom(new Vec2(ev.clientX-canvas.offsetLeft, ev.clientY-canvas.offsetTop), 2**(-ev.deltaY/100));
 };
--- src/viewport.js
+++ src/viewport.js
@@ -3,15 +3,25 @@
 import { Vec2 } from "/src/vec2.js";
 
 export class Viewport {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
+  constructor(canvasSize) {
+    this.pos = new Vec2();
     this.size = 1;
+    this.canvasSize = canvasSize;
   }
   toViewport(pos) {
-    return new Vec2((pos.x-innerWidth/2)/this.size+this.x, (innerHeight/2-pos.y)/this.size+this.y);
+    return new Vec2((pos.x-this.canvasSize.x/2)/this.size+this.pos.x, (this.canvasSize.y/2-pos.y)/this.size+this.pos.y);
   }
   fromViewport(pos) {
-    return new Vec2((pos.x-this.x)*this.size+innerWidth/2, innerHeight/2-(pos.y-this.y)*this.size);
+    return new Vec2((pos.x-this.pos.x)*this.size+this.canvasSize.x/2, this.canvasSize.y/2-(pos.y-this.pos.y)*this.size);
+  }
+  zoom(oldPosition, factor) {
+    console.log(oldPosition);
+    const lockPosition = this.toViewport(oldPosition);
+    this.size *= factor;
+    const newPosition = this.fromViewport(lockPosition);
+    const diff = new Vec2(oldPosition.x-newPosition.x, oldPosition.y-newPosition.y);
+    this.pos.x -= diff.x/this.size;
+    this.pos.y += diff.y/this.size;
+    const finalPosition = this.fromViewport(lockPosition);
   }
 }
